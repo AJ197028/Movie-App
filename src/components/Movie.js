9caad197cf993084e3ba46907af3286a648dc167
AJ197028 <aksjais20@gmail.com>
2023-04-07 16:28:42 +0530

    task 11

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,26 @@ import React from 'react';
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
+  console.log(props.movie);
+  const deleteHandler= async (e)=>{
+    e.preventDefault();
+    console.log(props.movie);
+    const response = await fetch(`https://react-http-8b260-default-rtdb.firebaseio.com/movies/${props.movie.id}.json`,{
+      method:"DELETE",
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+    console.log(response);
+    window.location.reload(false)
+  }
+
   return (
     <li className={classes.movie}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      <button style={{background:"red"}} onClick={deleteHandler}>Delete</button>
     </li>
   );
 };
